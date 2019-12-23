6488be3f9ae92db094ba61c2780c519739a42b97
itezaP <mshr900@gmail.com>
2019-12-23 09:23:37 +0900

    hoi

---
+++ service_worker.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'pwa-sample-cache';
+var urlsToCache = [
+	'/itezaP.github.io',
+]
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlToCache);
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
