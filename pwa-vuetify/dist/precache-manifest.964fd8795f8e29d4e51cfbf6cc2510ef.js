7135e48beed037d3ae36454690dcdab53b08a821
itezaP <mshr900@gmail.com>
2019-12-23 13:27:52 +0900

    gitignore設定忘れ

---
+++ pwa-vuetify/dist/precache-manifest.964fd8795f8e29d4e51cfbf6cc2510ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0471707248f07464e22a",
+    "url": "/css/chunk-vendors.40956daf.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f302463831412317a59d2530ea8cf3ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc7b41310664130e730",
+    "url": "/js/about.94d994c5.js"
+  },
+  {
+    "revision": "2607f350bb906f7c3c9c",
+    "url": "/js/app.433169c9.js"
+  },
+  {
+    "revision": "0471707248f07464e22a",
+    "url": "/js/chunk-vendors.7bd7c596.js"
+  },
+  {
+    "revision": "3a1bdb97bbf2361604fcdc47157bc534",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
